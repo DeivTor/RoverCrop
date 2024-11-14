4c04b238826b631fd1a02e149dfda0616276ccc5
Lizbarretor <rozolizeth23@gmail.com>
2024-11-14 09:55:02 -0500

    ecuaciones apli

--- controller/ecuaciones.py
+++ controller/ecuaciones.py
@@ -13,9 +13,24 @@ class funcionVelocidad:
 
     def calcularVelocidad (self, terreno):
         if (terreno == "Limoso"):
-            velocidad= 30
+
+            t = np.linspace(0, 0.25, 1000)
+            p = 66.67
+            K = 2963.8
+            Vf = 1
+            A = Vf*(K/p)
+            exp = -p*t
+
+            w = A*(1-pow(m.e,exp))
+
+            r=0.1
+            v = w * r
+            velocidad = v.ptp()
+            print(velocidad)
+
         elif(terreno == "Franco"):
             velocidad= 10
+
         return velocidad
     
     def establecerRuta (self, velocidad):
@@ -49,6 +64,9 @@ class funcionVelocidad:
                 {'direction': 'left', 'steps': 5},
                 {'direction': 'up', 'steps': 5},
             ]
+        elif(velocidad != 10 and velocidad !=30):
+            print("Prueba con las ecuaciones.")
+            
         return self.ruta
 
     
